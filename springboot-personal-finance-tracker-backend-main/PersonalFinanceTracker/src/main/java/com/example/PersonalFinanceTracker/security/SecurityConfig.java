bc1db6351bcdac54b2081746f4a485d2e60d455c
Nguyễn Hiếu An <194261813+hhieuann@users.noreply.github.com>
2026-03-04 19:38:20 +0700

    feature/signup

---
+++ springboot-personal-finance-tracker-backend-main/PersonalFinanceTracker/src/main/java/com/example/PersonalFinanceTracker/security/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.example.PersonalFinanceTracker.security;
+
+import com.example.PersonalFinanceTracker.security.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.*;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.*;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtFilter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(sess ->
+                        sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers("/api/auth/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
